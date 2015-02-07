af56170e4e48ad95a34b9ed5de421d355b9fa874
Guiom <scorpid@gmail.com>
2015-02-07 14:01:25 +0100

    stuff

--- client/main.js
+++ client/main.js
@@ -5,10 +5,14 @@ questionsByVotesHandle = Meteor.subscribeWithPagination('questionsByVotes', 10);
 questionsByTagHandle = Meteor.subscribeWithPagination('questionsByTag', Session.get("currentTag"), 10);
 
 Deps.autorun(function() {
-	Meteor.subscribe('singleQuestion', Session.get('currentQuestion'));
-	Meteor.subscribe('answersByQuestion', Session.get('currentQuestion'));
-	Meteor.subscribe('commentsByQuestion', Session.get('currentQuestion'));
+  Meteor.subscribe('singleQuestion', Session.get('currentQuestion'));
+  Meteor.subscribe('answersByQuestion', Session.get('currentQuestion'));
+  Meteor.subscribe('commentsByQuestion', Session.get('currentQuestion'));
 });
 
 Meteor.subscribe('notifications');
 Meteor.subscribe('tags');
+
+Meteor.startup(function() {
+  TAPi18n.setLanguage("es");
+});
--- client/views/users/user_page.js
+++ client/views/users/user_page.js
@@ -1,16 +1,6 @@
-Template.user.rendered=function() {
-  console.log("user_page rendered", this);
-};
-
 Template.user.helpers({
   fiveQuestions: function() {
-    console.log("fiveQuestions params: ",Session.get("userPageId"));
-    return Questions.find({ userId: Session.get("userPageId")});
-  },
-  upvotedQuestions: function() {
-    return Questions.find({ upvoters: {$in: [Session.get("userPageId")]} });
-  },
-  watchingQuestions: function() {
-    return Questions.find({ watchers: { $in: [Session.get("userPageId")]}});
+    console.log(this);
+    return Questions.find({ author: this.username});
   }
 });
