69530654973b687cb635f9cc30f67d5f81d4ae35
Guiom <scorpid@gmail.com>
2015-04-05 20:04:01 +0200

    Final? hah you wish

--- client/views/users/user_page.js
+++ client/views/users/user_page.js
@@ -2,5 +2,8 @@ Template.user.helpers({
   fiveQuestions: function() {
     console.log(this);
     return Questions.find({ author: this.username});
+  }, 
+  formatDate: function(dte) {
+    return new Date(dte).toString('dd/mm/YYYY');
   }
 });
